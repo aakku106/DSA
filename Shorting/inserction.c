55fae2f0e17387437e33a0b83077e308feebb139
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2024-12-08 08:39:50 +0545

    shorting(inserction) and searching

---
+++ Shorting/inserction.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int arr[] = {90, 2, 9, 2, 44, 6, 3};
+    int lenArr = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 1; i < lenArr; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (arr[j] > key && j >= 0)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        };
+        arr[j + 1] = key;
+    }
+    for(int i=0;i<lenArr;i++){
+        printf("%d\t",arr[i]);
+    }
+    return 0;
+}
---
+++ Shorting/oneBubbleSort.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void sort(int  array[],int lenArray){
+     for(int i=0;i<lenArray  ;i++){
+        for(int j=0;j<lenArray-1;j++){
+            if( array[j]> array[j+1]){
+                int  temp = array[j];
+                  array[j]=array[j+1];
+                 array[j+1]=  temp;
+            }
+        }
+    }   
+}
+int main(){
+int array[]={9,4,2,12,31213,5,7,8};
+    int lenArray=sizeof(array)/sizeof(array[0]);
+    sort( array,lenArray);
+    for(int i=0;i<lenArray;i++){
+        printf("%d\t",array[i]);}
+    return 0;
+}
+

7128007cfb7537e37d6d2bcd94f2aa4f09c14273
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2024-12-16 21:41:33 +0545

    Refactor merge sort implementation for improved readability and consistency

--- Shorting/mergShort.c
+++ Shorting/mergShort.c
@@ -60,10 +60,10 @@
 //     return 0;
 // }
 
-
 #include <stdio.h>
 
-void merge(int arr[], int l, int m, int r) {
+void merge(int arr[], int l, int m, int r)
+{
     int n1 = m - l + 1;
     int n2 = r - m;
 
@@ -74,44 +74,53 @@ void merge(int arr[], int l, int m, int r) {
     for (int j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
 
-   int i = 0;
+    int i = 0;
     int j = 0;
     int k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
             arr[k++] = L[i++];
         }
-        else {
+        else
+        {
             arr[k++] = R[j++];
         }
     }
 
-    while (i < n1) {
+    while (i < n1)
+    {
         arr[k++] = L[i++];
     }
 
-    while (j < n2) {
+    while (j < n2)
+    {
         arr[k++] = R[j++];
     }
 }
 
-void mergeSort(int arr[], int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = (r + l) / 2;
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
         merge(arr, l, m, r);
     }
 }
 
-void printArray(int arr[], int size) {
+void printArray(int arr[], int size)
+{
     // int i;
     for (int i = 0; i < size;)
         printf("%d ", arr[i++]);
     printf("\n");
 }
 
-int main() {
+int main()
+{
     int arr[] = {4, 7, 2, 5, 1, 9, 8, 6, 3};
     int arr_size = sizeof(arr) / sizeof(arr[0]);
 

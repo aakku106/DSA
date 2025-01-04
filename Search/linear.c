c19ee465d37d93e1f02fe41e520dcfdb460e73cb
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-01-05 01:34:48 +0545

    Implement linear search algorithm and add example usage

---
+++ Search/linear.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int len, int target)
+{
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {90, 2, 9, 2, 44, 6, 3};
+    int lenArr = sizeof(arr) / sizeof(arr[0]);
+    int target = 44;
+
+    int result = linearSearch(arr, lenArr, target);
+
+    if (result != -1)
+    {
+        printf("Element %d found at index %d.\n", target, result);
+    }
+    else
+    {
+        printf("Element %d not found in the array.\n", target);
+    }
+
+    printf("ProgramBy: AdarashaGaihre\n");
+
+    return 0;
+}

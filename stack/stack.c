9ed3a77834d91339aa63dd303b1526ead3ff06f6
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-01-04 20:44:10 +0545

    Refactor stack implementation: improve code readability, enhance user prompts, and streamline input handling

--- stack/stack.c
+++ stack/stack.c
@@ -1,66 +1,84 @@
-#include<stdio.h>
-#include <ctype.h>
-#include<stdlib.h>
+#include <stdio.h>
+// #include <ctype.h>
+#include <stdlib.h>
 #define MAX 10
 int stack[MAX];
-int pointer =-1;
-void push(int value){
-    if(pointer>MAX-1){
+int pointer = -1;
+void push(int value)
+{
+    if (pointer > MAX - 1)
+    {
         printf("Stack is full\n");
         return;
     }
-    else{
-        stack[pointer+1]=value;
+    else
+    {
+        stack[pointer + 1] = value;
         pointer++;
-        return;
     }
 }
-int pop(void){
-    if(pointer<0){
+int pop(void)
+{
+    if (pointer < 0)
+    {
         printf("\nThe Stack is Empty.\n");
-        return 0;
     }
-    else{
-        int throw=stack[pointer];
-        pointer--;
+    else
+    {
+        int throw = stack[pointer--];
         printf("\n");
         return throw;
     }
+    return 0;
 }
-void showStack(void){
-    for(int i=0;i<MAX;i++){
-        printf("%d\t",stack[i]);
+void showStack(void)
+{
+    printf("\n");
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("%d\t", stack[i]);
     }
     printf("\n");
 }
-int takeInput(void){
+int takeInput(void)
+{
     printf("\nEnter the Number:\t");
-        int input;
-        scanf("%d",&input);
+    int input;
+    scanf("%d", &input);
     return input;
 }
-void stop(void){
+void stop(void)
+{
     exit(0);
 }
-int main(void){
+int main(void)
+{
     showStack();
-    while(106){
-        printf("\nDo you wanna input something else in stack ?(y/no)\t");
+    while (106)
+    {
+        printf("\nDo you wanna input something else in stack ?(y/n)\t");
         char choose;
-        scanf("%s",&choose);
-        if(choose=='y'){
-            int input = takeInput();
-            push(input);
+        scanf("%s", &choose);
+        if (choose == 'y')
+        {
+            push(takeInput());
             showStack();
         }
-        printf("\nDo you wanna pop in stack ?(y/no)\t");
-            scanf("%s",&choose);
-            if(choose=='y'){
-               printf("%d",pop());
-                showStack();
-            }
-        printf("If You wanna exit loop press 'e' :>");  
-        if(choose=='e'){
+        printf("\nDo you wanna pop in stack ?(y/n)\t");
+        scanf("%s", &choose);
+        if (choose == 'y')
+        {
+            printf("%d", pop());
+            showStack();
+        }
+        else if (choose == 'n')
+        {
+            showStack();
+        }
+
+        printf("If You wanna exit loop press 'e' :>");
+        if (choose == 'e')
+        {
             stop();
         }
     }

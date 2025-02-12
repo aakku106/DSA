1500b85374a37c7a30ec45506a6fc284453efc17
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-02-12 15:20:52 +0545

    Add initial implementation of binary tree with basic structure and placeholder functions

---
+++ tree/implementationOfTree__SelfTry01__.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+	struct Node *left;
+	int data;
+	struct Node *right;
+}*head;
+
+void show(struct Node *pointer);
+int (int);
+
+void insertAtRight(int data){}
+void insertAtLeft(int data){}
+int deside(int data){
+}
+
+int main(void){
+	printf("___________");
+	for(int i=0;i<(sizeof(Nodes)/sizeof(Nodes[0]));i++){
+		if(i==0){
+			head->data=Nodes[i];
+		}
+	}
+}

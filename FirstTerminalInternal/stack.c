d8c99236a71c0a56b99ec03e72abc3fce6aa115a
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-04-28 01:38:48 +0545

    Formating indenting from 4 to 2

--- FirstTerminalInternal/stack.c
+++ FirstTerminalInternal/stack.c
@@ -1,28 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 #define MAX 10
 int stack[MAX];
-int pointer=-1;
+int pointer = -1;
 int pop(void);
 void push(int);
-int main(void){
-    push(10);
-    push(20);
-    pop();
-    printf("\nPop item is %d\t",pop());
-    printf("\nPop item is %d\t",pop());
+int main(void) {
+  push(10);
+  push(20);
+  pop();
+  printf("\nPop item is %d\t", pop());
+  printf("\nPop item is %d\t", pop());
 
-    return 0;
+  return 0;
 }
-void push(int value){
-    if(pointer==MAX-1)
-        printf("\nStack is full\n");
-    else
-        stack[++pointer]=value;
+void push(int value) {
+  if (pointer == MAX - 1)
+    printf("\nStack is full\n");
+  else
+    stack[++pointer] = value;
 }
-int pop(void){
-    if(pointer<0)
-        printf("\nstack is empty\n");
-    else
-        return stack[pointer--];
-    return -1;
+int pop(void) {
+  if (pointer < 0)
+    printf("\nstack is empty\n");
+  else
+    return stack[pointer--];
+  return -1;
 }

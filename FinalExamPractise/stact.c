2145208e46121c02c94df6a847b71c1d56d6c841
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-04-28 01:32:28 +0545

    main function to call others functions,with errorhandeling

--- FinalExamPractise/stact.c
+++ FinalExamPractise/stact.c
@@ -1,8 +1,24 @@
+#include <_stdlib.h>
 #include <stdio.h>
 #define MAX 3
 int stak[MAX], pop(), push(int), isEmpty(), isFull(), pointer;
 void peek();
 int main(void) {
   while (106) {
+    printf("Choose 1,2,3,4\n");
+    int choose, data;
+    scanf("%d", &choose);
+    switch (choose) {
+    case 1:
+      printf("\nENter number to put in stack\n");
+      scanf("%d", &data);
+      if (push(data) != -1)
+        printf("\n\t\t--->%d<---Pushed to stack's top\n", data);
+      break;
+    case 2:
+      data = pop();
+      if (data != -1)
+        printf("\n--->%d<---Poped out from top of stack\n", data);
+    }
   }
 }

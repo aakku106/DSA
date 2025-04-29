2f7d6d2c8a27d6a1e6c5ed530452a6b7e7240cdb
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-04-29 07:41:10 +0545

    Fixed number not found error adn changet space formating froom 4 to 2

--- Search/onesequentialSearch.c
+++ Search/onesequentialSearch.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
-#include<stdbool.h>
-int main(){
-    int numbers[]={1,3,2,4,5};
-    bool ifOutOfIndex=true;
-    printf("Enter number to search from array\t");
-    int numberToSearch;
-    int sizeOfArray=4;
-    scanf("%d",&numberToSearch);
-    for(int i=0;i<=sizeOfArray;i++){
-            if(numberToSearch==numbers[i]){
-                ifOutOfIndex=false;
-                printf("It was in the index\t%d",i);
-                break;
-        }    
+#include <stdbool.h>
+#include <stdio.h>
+int main() {
+  int numbers[] = {1, 3, 2, 4, 5};
+  bool ifOutOfIndex = false;
+  printf("Enter number to search from array\t");
+  int numberToSearch;
+  int sizeOfArray = 4;
+  scanf("%d", &numberToSearch);
+  for (int i = 0; i <= sizeOfArray; i++) {
+    if (numberToSearch == numbers[i]) {
+      ifOutOfIndex = true;
+      printf("It was in the index\t%d", i);
+      break;
     }
+  }
 
- if(numberToSearch){
-        printf("Number not found in array\n");
-    }
-    return 0;
+  if (!ifOutOfIndex) {
+    printf("\nNumber not found in array\n");
+  }
+  return 0;
 }

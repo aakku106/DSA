f3f6557c2835163f05554107e88778427d94d437
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-04-29 08:07:12 +0545

    refactor space,logic incomplete

--- Search/binTry.c
+++ Search/binTry.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
-int main()
-{
-    int nums[] = {1, 3,23,4, 5, 6};
-    int min = 0;
-    int numsSize = sizeof(nums) / sizeof(nums[0]);
-    int mid = (min + numsSize) / 2;
-    int target = 5;
-    while (min <= numsSize)
-    {
-        mid = (numsSize + min) / 2;
-        if (target < nums[mid])
-        {
-            numsSize = mid - 1;
-        }
+int main() {
+  int nums[] = {1, 3, 23, 4, 5, 6};
+  int min = 0;
+  int numsSize = sizeof(nums) / sizeof(nums[0]);
+  int mid = (min + numsSize) / 2;
+  int target = 5;
+  while (min <= numsSize) {
+    mid = (numsSize + min) / 2;
+    if (target < nums[mid]) {
+      numsSize = mid - 1;
+    }
 
-        else if (target > nums[mid])
-        {
-            min = mid + 1;
-        }
+    else if (target > nums[mid]) {
+      min = mid + 1;
+    }
 
-        else
-        {
-            break;
-        }
-    };
-           printf("%d",mid);
-    return 0;
+    else {
+      break;
+    }
+  };
+  printf("%d", mid);
+  return 0;
 }
- private void ccn(){
-    string wee = "ccn";:w
-
+private
+void ccn() {
+  string wee = "ccn";:w
 }

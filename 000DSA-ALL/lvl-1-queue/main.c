45173e40d2579e4ec49e9df596376a0696345724
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-06 00:36:15 +0545

    People are scarede of go so i removed it for their sake
    many people seems to be just scared of power of goto,
    so i removed the use of goto and made
    modular approach Dijkstra's way may be

--- 000DSA-ALL/lvl-1-queue/main.c
+++ 000DSA-ALL/lvl-1-queue/main.c
@@ -1,12 +1,12 @@
 #include "CircularQueue.h"
 #include "LinearQueue.h"
 #include <stdio.h>
-
+void linearqueue(void);
+void circularqueue(void);
+int choice, choose;
 int main() {
-  int choice, choose;
 
   while (106) {
-  a:
     printf("\nChoose Data Structure:\n");
     printf("\n|\t\t--1. Linear Queue\n|\t\t2. Circular Queue\n|\t\t0. Exit\n");
     printf("Enter choice---->\t");
@@ -14,73 +14,12 @@ int main() {
 
     switch (choice) {
     case 1:
-      while (106) {
-        printf("Enter ur choice\n\t\t1. Enqueue\n\t\t2. Dequeue\n\t\t3. "
-               "peek\n\t\t0. choose ZERO to colse program\n\t\t4. TO Exit "
-               "LinearQueue");
-        int data;
-        scanf("%d", &choose);
-        switch (choose) {
-        case 1:
-          printf("\nEnter num to Enqueue\n");
-          scanf("%d", &data);
-          if (EnQueue(data) != -1)
-            printf("\n|---\t\t%d<----Enqueues---|\n", data);
-          break;
-        case 2:
-          data = DeQueue();
-          if (data != -1)
-            printf("\n|---\t\t%d<----DEQUYED---|\n", data);
-          break;
-        case 3:
-          Lpeek();
-          break;
-        case 0:
-          printf("Exiting...\n");
-          return 0;
-        case 4:
-          goto a;
-          break;
-        default:
-          printf("\n\t\tCHOOE CORRECT OPTION\n");
-        }
-      }
+      linearqueue();
     case 2:
-      while (106) {
-        printf("Choose betn:\n\t\t1. TO enqueue\n\t\t2. TO dequeue\n\t\t3.To "
-               "Peek\n\t\t4.To exit Circular queue\n\t\t0. To exit program\n");
-        int choose, data;
-        scanf("%d", &choose);
-        switch (choose) {
-        case 1:
-          printf("\t\tEnter number to qneueue\n");
-          scanf("%d", &data);
-          if (Enqueue(data) != -1)
-            printf("%d<---Enqueued\n", data);
-          break;
-        case 2:
-          data = Dequeue();
-          if (data != -1)
-            printf("%d----Dequeued\n", data);
-          break;
-        case 3:
-          Cpeek();
-          break;
-        case 4:
-          goto a;
-          break;
-        case 0:
-          printf("Exiting...\n");
-          return 0;
-        default:
-          printf("Chose correct wefkopwfk\n");
-        }
-      }
-
+      circularqueue();
     case 0:
       printf("Exiting...\n");
       return 0;
-
     default:
       printf("Invalid choice!\n");
       break;
@@ -88,4 +27,70 @@ int main() {
   }
 
   return 0;
+}
+void linearQueue(void) {
+  while (106) {
+    printf("Enter ur choice\n\t\t1. Enqueue\n\t\t2. Dequeue\n\t\t3. "
+           "peek\n\t\t0. choose ZERO to colse program\n\t\t4. TO Exit "
+           "LinearQueue");
+    int data;
+    scanf("%d", &choose);
+    switch (choose) {
+    case 1:
+      printf("\nEnter num to Enqueue\n");
+      scanf("%d", &data);
+      if (EnQueue(data) != -1)
+        printf("\n|---\t\t%d<----Enqueues---|\n", data);
+      break;
+    case 2:
+      data = DeQueue();
+      if (data != -1)
+        printf("\n|---\t\t%d<----DEQUYED---|\n", data);
+      break;
+    case 3:
+      Lpeek();
+      break;
+    case 0:
+      printf("Exiting...\n");
+      return;
+    case 4:
+      main();
+      break;
+    default:
+      printf("\n\t\tCHOOE CORRECT OPTION\n");
+    }
+  }
+}
+
+void circularqueue(void) {
+  while (106) {
+    printf("Choose betn:\n\t\t1. TO enqueue\n\t\t2. TO dequeue\n\t\t3.To "
+           "Peek\n\t\t4.To exit Circular queue\n\t\t0. To exit program\n");
+    int choose, data;
+    scanf("%d", &choose);
+    switch (choose) {
+    case 1:
+      printf("\t\tEnter number to qneueue\n");
+      scanf("%d", &data);
+      if (Enqueue(data) != -1)
+        printf("%d<---Enqueued\n", data);
+      break;
+    case 2:
+      data = Dequeue();
+      if (data != -1)
+        printf("%d----Dequeued\n", data);
+      break;
+    case 3:
+      Cpeek();
+      break;
+    case 4:
+      main();
+      break;
+    case 0:
+      printf("Exiting...\n");
+      return;
+    default:
+      printf("Chose correct wefkopwfk\n");
+    }
+  }
 }

3dda0797a44283e42893878ebef3c1fb126e7cc4
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-08 23:58:49 +0545

    DEfined .c and .h files

---
+++ 000DSA-ALL/lvl-2-stack/stack.c
@@ -0,0 +1,61 @@
+#include "stack.h"
+#include <stdio.h>
+int stack[MAX], pointer = -1;
+
+void stack_main_call(void) {
+  while (106) {
+    printf("Choose 1,2,3,4\n");
+    int choose, data;
+    scanf("%d", &choose);
+    switch (choose) {
+    case 1:
+      printf("\nENter number to put in stack\n");
+      scanf("%d", &data);
+      if (push(data) != -1)
+        printf("\n\t\t--->%d<---Pushed to stack's top\n", data);
+      break;
+    case 2:
+      data = pop();
+      if (data != -1)
+        printf("\n--->%d<---Poped out from top of stack\n", data);
+      break;
+    case 3:
+      peek();
+      break;
+    case 4:
+      return;
+    default:
+      printf("\t\tChoose correct optn\n");
+    }
+  }
+  return;
+}
+int push(int data) {
+  if (isFull() != -1)
+    return stack[++pointer] = data;
+  printf("Stack is full------XXXXXXXX\n");
+  return -1;
+}
+int pop(void) {
+  if (isEmpty() != -1)
+    return stack[pointer--];
+  printf("Stack is empty ---------XXXXXXXXXXx\n");
+  return -1;
+}
+int isFull(void) {
+  if (pointer >= MAX - 1)
+    return -1;
+  return 0;
+}
+int isEmpty(void) {
+  if (pointer < 0)
+    return -1;
+  return 0;
+}
+void peek(void) {
+  if (isEmpty() != -1) {
+    printf("\t\t--->%d<---\n", stack[pointer]);
+    return;
+  }
+  printf("Its empty \n");
+}
---
+++ 000DSA-ALL/lvl-2-stack/stack.h
@@ -0,0 +1,7 @@
+#ifndef STACK_H
+#define STACK_H
+#define MAX 15
+int pop(void), push(int), isEmpty(void), isFull(void);
+void peek(void);
+void stack_main_call(void);
+#endif

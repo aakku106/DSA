0a572f0a536dd0bf0dd0882ddb540db5c1252b3e
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-09 00:23:51 +0545

    added formating and colors

--- 000DSA-ALL/main.c
+++ 000DSA-ALL/main.c
@@ -1,15 +1,26 @@
+#include "ClearScreen.h"
+#include "color.h"
 #include "lvl-1-queue/Queuemain.h"
 #include "lvl-2-stack/stack.h"
 #include <stdio.h>
 #include <sys/_types/_sigaltstack.h>
+#include <unistd.h>
 
 int main() {
-  printf("Choose\n");
+  clear_screen();
+  printf(CRYON "\n\t\t_-______-_CHOOSE BETWEEN_-________-_ \n" RESET);
+  usleep(50000);
+  printf(YELLOW "\n\t\t1.STACK\n" RESET);
+  usleep(50000);
+  printf(YELLOW "\n\t\t2.QUEUE\n" RESET);
+  usleep(50000);
+  printf(CYAN "\t\t4. To exit Circular queue\n" RESET);
+  usleep(50000);
+  printf(RED "\t\t0.To exit program\n " RESET);
   int choice;
   scanf("%d", &choice);
   switch (choice) {
   case 1:
-
     queue_main_call();
     break;
   case 2:

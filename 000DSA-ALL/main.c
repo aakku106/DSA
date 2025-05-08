f00d04642e7b5dfeada0057715c8ea82d7ebbe76
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-09 00:37:32 +0545

    Added while loop and some extra conditions

--- 000DSA-ALL/main.c
+++ 000DSA-ALL/main.c
@@ -3,28 +3,38 @@
 #include "lvl-1-queue/Queuemain.h"
 #include "lvl-2-stack/stack.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/_types/_sigaltstack.h>
 #include <unistd.h>
 
-int main() {
+int main(void) {
   clear_screen();
-  printf(CRYON "\n\t\t_-______-_CHOOSE BETWEEN_-________-_ \n" RESET);
-  usleep(50000);
-  printf(YELLOW "\n\t\t1.STACK\n" RESET);
-  usleep(50000);
-  printf(YELLOW "\n\t\t2.QUEUE\n" RESET);
-  usleep(50000);
-  printf(CYAN "\t\t4. To exit Circular queue\n" RESET);
-  usleep(50000);
-  printf(RED "\t\t0.To exit program\n " RESET);
-  int choice;
-  scanf("%d", &choice);
-  switch (choice) {
-  case 1:
-    stack_main_call();
-    break;
-  case 2:
-    queue_main_call();
+  while (106) {
+
+    printf(CRYON "\n\t\t_-______-_CHOOSE BETWEEN_-________-_ \n" RESET);
+    usleep(50000);
+    printf(YELLOW "\n\t\t1.STACK\n" RESET);
+    usleep(50000);
+    printf(YELLOW "\n\t\t2.QUEUE\n" RESET);
+    usleep(50000);
+    printf(CYAN "\t\t4. To exit ADT\n" RESET);
+    usleep(50000);
+    printf(RED "\t\t0.To exit program\n " RESET);
+    int choice;
+    scanf("%d", &choice);
+    switch (choice) {
+    case 1:
+      stack_main_call();
+      break;
+    case 2:
+      queue_main_call();
+
+    case 4:
+      return 0;
+    case 0:
+      exit(0);
+    default:
+      printf(RED "\t\t ?-?-?---------Choose correct optn-------!!!\n" RESET);
+    }
   }
-  return 0;
 }

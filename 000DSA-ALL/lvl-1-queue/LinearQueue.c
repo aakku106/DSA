b201b241d4fdb34bf751edb0b709f5e8d4c56d9d
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-09 00:54:35 +0545

    Fixing formating and typos

--- 000DSA-ALL/lvl-1-queue/LinearQueue.c
+++ 000DSA-ALL/lvl-1-queue/LinearQueue.c
@@ -1,38 +1,42 @@
 #include "LinearQueue.h"
+#include "../ClearScreen.h"
+#include "../color.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int front = 0, rear = -1, queue[MAX];
 
 void linear_queue(void) {
+  clear_screen();
   while (106) {
-    printf("Enter ur choice\n\t\t1. Enqueue\n\t\t2. Dequeue\n\t\t3. "
-           "peek\n\t\t0. choose ZERO to colse program\n\t\t4. TO Exit "
-           "LinearQueue");
+    printf(CRYON "Enter ur choice\n\t\t1. Enqueue\n\t\t2. Dequeue\n\t\t3. "
+                 "peek\n\t\t0. choose ZERO to colse program\n\t\t4. TO Exit "
+                 "LinearQueue" RESET);
     int choose, data;
     scanf("%d", &choose);
     switch (choose) {
     case 1:
-      printf("\nEnter num to Enqueue\n");
+      printf(YELLOW "\nEnter num to Enqueue\n");
       scanf("%d", &data);
       if (EnQueue(data) != -1)
-        printf("\n|---\t\t%d<----Enqueues---|\n", data);
+        printf(GREEN "\n|---\t\t%d<----Enqueues---|\n", data);
       break;
     case 2:
       data = DeQueue();
       if (data != -1)
-        printf("\n|---\t\t%d<----DEQUYED---|\n", data);
+        printf(GREEN "\n|---\t\t%d<----DEQUYED---|\n", data);
       break;
     case 3:
       Lpeek();
       break;
     case 0:
-      printf("Exiting...\n");
+      printf(RED "Exiting...\n" RESET);
       exit(0);
     case 4:
+      clear_screen();
       return;
     default:
-      printf("\n\t\tCHOOE CORRECT OPTION\n");
+      printf(RED "\n\t\tCHOOE CORRECT OPTION\n" RESET);
     }
   }
 }
@@ -40,13 +44,13 @@ void linear_queue(void) {
 int EnQueue(int data) {
   if (IsFull() != -1)
     return queue[++rear] = data;
-  printf("\nXXX---Queue is full---XXX\n");
+  printf(RED "\nXXX---Queue is full---XXX\n");
   return -1;
 }
 int DeQueue(void) {
   if (IsEmpty() != -1)
     return queue[front++];
-  printf("\nXXX___Queue is empty---XXX\n");
+  printf(RED "\nXXX___Queue is empty---XXX\n");
   return -1;
 }
 int IsFull(void) {
@@ -61,8 +65,8 @@ int IsEmpty(void) {
 }
 void Lpeek(void) {
   if (IsEmpty() != -1) {
-    printf("\n\t\t|---%d<---Is In Front---|\n", queue[front]);
+    printf(GREEN "\n\t\t|---%d<---Is In Front---|\n", queue[front]);
     return;
   }
-  printf("\nXXX---QUEUE is EMPTY---XXX\n");
+  printf(RED "\nXXX---QUEUE is EMPTY---XXX\n" RESET);
 }

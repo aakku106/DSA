496f450933df07dcc80e4ae446c9de69ad8f92aa
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-14 15:11:50 +0545

    Added qudratic probing

---
+++ Hash/QudraticProbing.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+int has_function(int, int);
+int main(void) {
+  printf("\nInitilize the size of table(DEFULT 10)\t");
+  int sizeOfTable = 10;
+  scanf("%d", &sizeOfTable);
+  int *hasTable = calloc(sizeOfTable, sizeof(int));
+  for (int i = 0; i < sizeOfTable;)
+    hasTable[i++] = -1;
+  int key;
+  printf("\nhow many input do we have ??\t");
+  int input;
+  scanf("%d", &input);
+  for (int i = 0; i <= input; i++) {
+    printf("\n\tEnter key\t");
+    scanf("%d", &key);
+    int index = has_function(key, sizeOfTable), j = 0, oIndex = index;
+    while (hasTable[index] != -1) {
+      printf("\nCollision occured at %d, trying to resolve...\n", index);
+      ++j;
+      index =
+          (oIndex + j * j) % sizeOfTable; // Circular movment to next same like
+                                          // circular quque but in sus way
+      if (hasTable[index] > sizeOfTable + 1) {
+        printf(
+            "\nNo space in array allocated by you, so %d can be inserted...\n",
+            key);
+        return 0;
+      }
+    }
+    if (hasTable[index] == -1)
+      hasTable[index] = key;
+    printf("\n");
+    for (int k = 0; k < sizeOfTable; k++)
+      printf("\t%d", hasTable[k]);
+  }
+  free(hasTable);
+  return 0;
+}
+int has_function(int key, int size) { return key % size; }
